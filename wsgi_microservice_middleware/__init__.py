6d029377dd92090dcf8ff046ab61d02356d38457
Kevin Hannegan <kevin@chart-a-lot.com>
2020-03-17 03:49:51 +0000

    update __init__.py

--- wsgi_microservice_middleware/__init__.py
+++ wsgi_microservice_middleware/__init__.py
@@ -1,8 +1,19 @@
 import environs
 
-
 env = environs.Env()
-
-
 env.read_env()
+
+from wsgi_microservice_middleware.cors import CORSMiddleware
+from wsgi_microservice_middleware.request_id import (
+    RequestIdFilter, 
+    RequestIdMiddleware, 
+    current_request_id
+)
+
+__all__ = [
+    'CORSMiddleware',
+    'RequestIdFilter', 
+    'RequestIdMiddleware', 
+    'current_request_id'
+]
 
